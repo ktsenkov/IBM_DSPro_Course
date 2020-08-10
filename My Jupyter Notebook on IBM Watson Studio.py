7a8e13e8ccdaa9239403bb6fbe9dd17a4c241df1
ktsenkov <66933631+ktsenkov@users.noreply.github.com>
2020-08-10 18:39:28 +0300

    Update My Jupyter Notebook on IBM Watson Studio.py

--- My Jupyter Notebook on IBM Watson Studio.py
+++ My Jupyter Notebook on IBM Watson Studio.py
@@ -1,1 +1,41 @@
+# My Jupyter Notebook on IBM Watson Studio
+
+### **Kalin H. Tsenkov**
+
+Ideally, I would wish I was more satisfied with this course.
+
+I am interested in data science because I would like to add more knowledge to my current skill set.
+
+### The code cell below executes a code to determine what is the sum of 2 and 2 and prints the result.
+
+val_1 = 2
+val_2 = 2
+
+result = val_1 + val_2 
+
+print(result)
+
+#### Horizontal Rule
+
+1. Hyphens
+
+---
+
+2. Asterisks
+
+***
+
+3. Underscores
+
+___
+
+#### Lists
+
+1. First ordered list item
+2. Another item
+  * Unordered sub-list, 
+  * Another unordered sub-list.
+1. Actual numbers don't matter, just that it's a number
+  1. Ordered sub-list
+4. And another item.
 
